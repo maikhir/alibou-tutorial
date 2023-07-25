c9b6a2cad9abab77e89833e57da39eb2f0224199
Hirthe Maik 8DM <maik.hirthe@rohde-schwarz.com>
2023-07-25 07:07:19 +0200

    Initial commit

---
+++ config-server/src/main/java/de/hirthe/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package de.hirthe.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}

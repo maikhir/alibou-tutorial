60c199b235f07cfd4c92c01b5e10fa86bc542546
HIRTHE <maik.hirthe@gmx.de>
2023-08-04 11:20:28 +0200

    finished with gateway

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ here is the master
 ![img.png](resources/img.png)
 
 current status: begin with gateway https://www.youtube.com/watch?v=KJ0cSvYj41c&list=PL41m5U3u3wwm-27nQk1rUqJRIRtnUoqMj
+
+start with external config: https://www.youtube.com/watch?v=KJ0cSvYj41c&list=PL41m5U3u3wwm-27nQk1rUqJRIRtnUoqMj
--- school/src/main/java/de/hirthe/school/client/StudentClient.java
+++ school/src/main/java/de/hirthe/school/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "student-service", url = "http://localhost:8090/api/v1/students")
+@FeignClient(name = "student-service", url = "${application.config.students-url}")
 public interface StudentClient {
 
     @GetMapping("/school/{school-id}")
